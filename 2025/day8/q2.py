b10797544a766fa2b86b06fa943eb86c58f67b92
Priyanshu Saini <priyanshusaini124@gmail.com>
2025-12-28 14:43:13 +0530

    AoC

--- 2025/day8/q2.py
+++ 2025/day8/q2.py
@@ -1,4 +1,3 @@
-import math
 import heapq
 
 def find_distance(a,b):
@@ -18,61 +17,50 @@ for i in range(len(input_data)-1):
     for j in range(i+1, len(input_data)):
         distance[(i,j)] = find_distance(input_data[i],input_data[j])
 
+# print(f'len of distance = {len(distance)}')
 # print(distance)
-sorted_distance = heapq.nsmallest(10, distance.items(), key=lambda x: x[1])
+sorted_distance = heapq.nsmallest(len(distance), distance.items(), key=lambda x: x[1])
+
+
+# for i, (k,v) in enumerate(sorted_distance):
+#     print(i)
+#     print(k[0],k[1],input_data[k[0]],input_data[k[1]],v)
+#     print()
 
 # print('\nSorted Dist:')
 # print(sorted_distance)
 
-final  = []
+final = []
+last = 0
 
-
-
-for k,v in sorted_distance:
+for k, v in sorted_distance:
     a = -1
     b = -1
 
-    for i,s in enumerate(final):
+    for i, s in enumerate(final):
         if k[0] in s:
             a = i
-            
         if k[1] in s:
-             b = i
-             
+            b = i
+
     if a == -1 and b == -1:
         final.append({k[0], k[1]})
+        last = input_data[k[0]][0] * input_data[k[1]][0]
+
     elif a != -1 and b == -1:
         final[a].add(k[1])
+        last = input_data[k[0]][0] * input_data[k[1]][0]
+
     elif a == -1 and b != -1:
         final[b].add(k[0])
+        last = input_data[k[0]][0] * input_data[k[1]][0]
+
     elif a != b:
         hi, lo = max(a, b), min(a, b)
         final[lo].update(final[hi])
         final.pop(hi)
+        last = input_data[k[0]][0] * input_data[k[1]][0]
 
-#     print(f'on {k[0],k[1]} appending: {final}')  
-            
-print(f'final list: {final}')   
-
-x = 0
-y = 0
-z = 0
-
-for i in final:
-    length = len(i)
-
-    if(length > x):
-        z = y
-        y = x
-        x = length
-    
-    elif(length > y):
-        z = y
-        y = length
-
-    elif length > z:
-        z = length
-
-print(f'x = {x}, y = {y}, z = {z}')
-
-print (x*y*z)
+    if len(final) == 1 and len(final[0]) == len(input_data):
+        print(f"final answer = {last}")
+        break
